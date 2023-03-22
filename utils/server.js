a2f5c3f2c37b26c716b3e89b2e9812ae8b89cfd5
Devroy Blake <necrodevz@gmail.com>
2023-03-22 15:36:52 -0500

    cosmetic changes to code in lighthouserc.js

--- utils/server.js
+++ utils/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
-const port = process.env.PORT || 8080
+const app = express();
+const port = process.env.PORT || 8080;
 
 // sendFile will go here
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.html'))
-})
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
 
-app.listen(port)
-console.log(`Server started at http://localhost:${port}`)
+app.listen(port);
+console.log(`Server started at http://localhost:${port}`);
