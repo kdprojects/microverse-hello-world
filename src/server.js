7997bc40ea695ffa14aad6637664db54a57cc279
Devroy Blake <necrodevz@gmail.com>
2023-03-22 14:56:08 -0500

    fix style error in server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import path from 'path';
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const port = process.env.PORT || 8080;
+const app = express()
+const port = process.env.PORT || 8080
 
 // sendFile will go here
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
 
-app.listen(port);
-console.log(`Server started at http://localhost:${port}`);
+app.listen(port)
+console.log(`Server started at http://localhost:${port}`)
