ff5c511f12351e895752dcd7bfb5b41d4db146ad
Devroy Blake <necrodevz@gmail.com>
2023-03-24 16:31:00 -0500

    update files

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../src/index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.use('/', express.static('./src/'))
+// app.use('/', express.static('./src/'))
 
 app.listen(port);
 console.log(`Server started at http://localhost:${port}`);
